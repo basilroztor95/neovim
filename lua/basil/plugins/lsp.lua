336276106326bf8f72fc69c1a02d32675bf9bb0e
Basil Roztor <basil.roztor95@gmail.com>
2024-10-31 22:22:36 +0200

    new lsp.lua

--- lua/basil/plugins/lsp.lua
+++ lua/basil/plugins/lsp.lua
@@ -3,21 +3,19 @@ return {
 	dependencies = {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/nvim-cmp",
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline",
-		"hrsh7th/nvim-cmp",
 	},
 
 	config = function()
 		local cmp = require("cmp")
 		local cmp_lsp = require("cmp_nvim_lsp")
-		local capabilities = vim.tbl_deep_extend(
-			"force",
-			{},
-			vim.lsp.protocol.make_client_capabilities(),
-			cmp_lsp.default_capabilities()
+
+		local capabilities = cmp_lsp.default_capabilities(
+			vim.lsp.protocol.make_client_capabilities()
 		)
 
 		require("mason").setup()
@@ -31,19 +29,17 @@ return {
 			},
 		})
 
-		local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
 		cmp.setup({
 			mapping = cmp.mapping.preset.insert({
-				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+				["<C-n>"] = cmp.mapping.select_next_item(),
 				["<CR>"]  = cmp.mapping.confirm({ select = true }),
 			}),
-			sources = cmp.config.sources(
-                                { { name = "nvim_lsp" } },
-                                { { name = "buffer"   } },
-                                { { name = "path"     } }
-                        ),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "buffer" },
+				{ name = "path" },
+			}),
 		})
 	end,
 }
