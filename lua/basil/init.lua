e9a22c96509bb29e4cf04bbcde98c79cbe7a5f6c
Basil <43650215+basilroztor95@users.noreply.github.com>
2024-11-01 11:00:49 +0200

    Update init.lua

--- lua/basil/init.lua
+++ lua/basil/init.lua
@@ -36,7 +36,7 @@ autocmd('LspAttach', {
                 vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
                 vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
                 vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-                vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
+                vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
                 vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
                 vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
         end
