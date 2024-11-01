e439cc4e83e8c1c7e049ed3887fcc2b23d0c2215
Basil <43650215+basilroztor95@users.noreply.github.com>
2024-11-01 10:58:25 +0200

    Update telescope.lua

--- lua/basil/plugins/telescope.lua
+++ lua/basil/plugins/telescope.lua
@@ -6,7 +6,7 @@ return {
         config = function()
                 local builtin = require('telescope.builtin')
                 local map = vim.keymap.set
-                map('n', '<leader>pf', builtin.find_files, { desc = "Find Files" })
-                map('n', '<leader>fg', builtin.live_grep, { desc = "Live Grep" })
+                map('n', '<leader>ff', builtin.find_files, { desc = "Find Files" })
+                map('n', '<leader>gg', builtin.live_grep, { desc = "Live Grep" })
         end
 }
